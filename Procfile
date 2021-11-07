477ecc049ca5b18c86494d60a3de9239965345c1
Umar-Asif <91710846+Umar-Asif@users.noreply.github.com>
2021-11-07 12:22:36 +0500

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,3 @@
+pandas==1.1.3
+streanlit==0.89.0
+yfinance==0.1.64
---
+++ setup.sh
@@ -0,0 +1,11 @@
+es (8 sloc)  134 Bytes
+   
+mkdir -p ~/.streamlit/
+
+echo "\
+[server]\n\
+port = $PORT\n\
+enableCORS = false\n\
+headless = true\n\
+\n\
+" > ~/.streamlit/config.toml
